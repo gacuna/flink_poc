e17f8c136737591e888e3f146ce9b04f6b4b741b
Lucas Campana Levy <luke8377@circe.Home>
2024-07-29 20:19:58 +0100

    add stuff

---
+++ src/main/resources/jobs/job.sql
@@ -0,0 +1,8 @@
+SET 'table.local-time-zone' = 'UTC';
+
+SET 'table.exec.mini-batch.enabled' = 'true';
+
+SET 'table.exec.mini-batch.allow-latency' = '500 ms';
+
+SET 'table.exec.mini-batch.size' = '1000';
+
