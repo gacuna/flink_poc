7f8d09a1491949db85636c38c22694246d4a4826
Lucas Campana <lucas.campana-contractor@fabfitfun.com>
2024-07-30 01:24:21 +0100

    add stuff

--- ext/jobs/job.sql
+++ ext/jobs/job.sql
@@ -1,9 +1,6 @@
 SET 'table.local-time-zone' = 'UTC';
-
 SET 'table.exec.mini-batch.enabled' = 'true';
-
 SET 'table.exec.mini-batch.allow-latency' = '500 ms';
-
 SET 'table.exec.mini-batch.size' = '1000';
 
 CREATE TABLE ebdb_users (
